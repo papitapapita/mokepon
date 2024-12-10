e8eaa555f232a2a74bb3a9acbb8d370eabdd4166
Jonathan <many.d.r.o.2001@hotmail.com>
2024-12-09 19:30:21 -0500

    Prettier and linter added and code prettied@

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-class GameCharacter{
-    constructor(id, pet){
-        this.id = id;
-        this.pet = pet;
-        this.position = {
-            x: 0,
-            y: 0
-        };
-    }
+class GameCharacter {
+  constructor(id, pet) {
+    this.id = id;
+    this.pet = pet;
+    this.position = {
+      x: 0,
+      y: 0
+    };
+  }
 }
 
-class Mokepon{
-    constructor(name){
-        this.name = name;
-    }
+class Mokepon {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 const playersList = [];
@@ -24,39 +24,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/join", (req, res) => {
-    const player = new GameCharacter(`${Math.floor(Math.random() * 100)}`);
-    playersList.push(player);
-    res.send(player.id);
+app.get('/join', (req, res) => {
+  const player = new GameCharacter(
+    `${Math.floor(Math.random() * 100)}`
+  );
+  playersList.push(player);
+  res.send(player.id);
 });
 
-app.get("/players", (req, res) => {
-    console.log(playersList);
-    res.send(JSON.stringify(playersList));
-})
-
-app.post("/mokepon/:playerId", (req, res) => {
-    const playerId = req.params.playerId || "";
-    const mokeponData = req.body.mokepon || "";
-    //const mokepon = new Mokepon(mokeponData.name);
-    const playerIndex = playersList.findIndex(player => player.id == playerId);
-    if(playerIndex >= 0){
-        playersList[playerIndex].pet = mokeponData;
-    }
-    res.end();
+app.get('/players', (req, res) => {
+  console.log(playersList);
+  res.send(JSON.stringify(playersList));
 });
 
-app.post("/mokepon/:playerId/position", (req, res) => {
-    const playerId = req.params.playerId || "";
-    const playerIndex = playersList.findIndex(player => player.id == playerId);
-    if(playerIndex >= 0){
-        const position = {x: req.body.x, y: req.body.y} || {};
-        playersList[playerIndex].position = position;
-    }
-    res.end();
+app.post('/mokepon/:playerId', (req, res) => {
+  const playerId = req.params.playerId || '';
+  const mokeponData = req.body.mokepon || '';
+  //const mokepon = new Mokepon(mokeponData.name);
+  const playerIndex = playersList.findIndex(
+    (player) => player.id == playerId
+  );
+  if (playerIndex >= 0) {
+    playersList[playerIndex].pet = mokeponData;
+  }
+  res.end();
 });
 
+app.post('/mokepon/:playerId/position', (req, res) => {
+  const playerId = req.params.playerId || '';
+  const playerIndex = playersList.findIndex(
+    (player) => player.id == playerId
+  );
+  if (playerIndex >= 0) {
+    const position = { x: req.body.x, y: req.body.y } || {};
+    playersList[playerIndex].position = position;
+  }
+  res.end();
+});
 
 app.listen(5002, () => {
-    console.log('Server Working');
+  console.log('Server Working');
 });
